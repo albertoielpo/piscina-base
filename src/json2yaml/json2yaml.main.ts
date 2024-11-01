ed8098c87b85f13ec5f8834b628bf665b138b26a
Alberto Ielpo <alberto.ielpo@gmail.com>
2024-11-01 22:17:05 +0100

    Other example

--- src/json2yaml/json2yaml.main.ts
+++ src/json2yaml/json2yaml.main.ts
@@ -1,12 +1,14 @@
 import { Logger } from "@nestjs/common";
+import { writeFile } from "node:fs/promises";
+import { printMemUsage } from "../format.utils";
 import Json2Yaml from "./json2yaml.service";
 
-async function fetchRemoteJson(): Promise<ArrayBuffer> {
+async function fetchRemoteJson(type: "small" | "large"): Promise<ArrayBuffer> {
     const jsonSmall = "https://jsonplaceholder.typicode.com/todos/1";
     const jsonLarge =
         "https://raw.githubusercontent.com/json-iterator/test-data/refs/heads/master/large-file.json";
 
-    const res = await fetch(jsonLarge, {
+    const res = await fetch(type === "small" ? jsonSmall : jsonLarge, {
         method: "GET",
         signal: AbortSignal.timeout(30_000)
     });
@@ -15,27 +17,21 @@ async function fetchRemoteJson(): Promise<ArrayBuffer> {
 }
 
 // exec convertion from json to yaml
-// the idea is to test big json data
 (async () => {
     const logger = new Logger(`Main`);
     logger.log("application start start");
     const service = new Json2Yaml();
 
-    const json1 = {
-        how: 1,
-        are: true,
-        you: ["fine", "thanks!"]
-    };
-
-    const json2 = await fetchRemoteJson();
-    const data = [json2];
+    const json1 = await fetchRemoteJson("small");
+    const json2 = await fetchRemoteJson("large");
+    const data = [json1, json2];
+    printMemUsage(logger);
     const yamls = await Promise.all(data.map((x) => service.convert(x)));
-    //     printMemUsage(logger);
+    printMemUsage(logger);
 
-    // for (let ii = 0; ii < data.length; ii++) {
-    //     logger.log(data[ii]);
-    //     logger.log(yamls[ii]);
-    // }
+    for (let ii = 0; ii < data.length; ii++) {
+        writeFile(`res-${ii}.yml`, yamls[ii]).catch((err) => console.log(err));
+    }
 
     logger.log("end");
 })();
